ddcd1d252ad78fa520a1fad02fbea11751314147
Connor McCann <cjmccann@bu.edu>
2017-01-02 18:41:18 -0500

    adding screen shots

--- website/db.js
+++ website/db.js
@@ -17,7 +17,7 @@ exports.connect = function(mode, done) {
   state.pool = mysql.createPool({
     host: 'cloud9.myftp.biz',
     user: 'owncloud',
-    password: '@BuEcE2017',
+    password: '***',
     database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
   });
 
