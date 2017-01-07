d4fe9c90fda3c4006242135bd162c9884f6116bf
Connor McCann <cjmccann@bu.edu>
2017-01-07 12:11:30 -0500

    update for nested sql query

--- website/public/javascripts/products.js
+++ website/public/javascripts/products.js
@@ -9,107 +9,109 @@ $(document).ready(function () {
         $.get('/get_price_point', function(asin_data) {
             console.log(asin_data);
             raw_asin = asin_data;
-        });
-        
-        $.get('/get_product_data',function(returned_data) {
-            // cool trick to get the ID for the tbody within the table tag !!
-            var tbody = $("#prod_table > tbody:last");
 
-            for (var row in returned_data) {
-                // log data to webpage
-                console.log(returned_data[row]);
+            // Query the db for all product data
+            $.get('/get_product_data',function(returned_data) {
+                // cool trick to get the ID for the tbody within the table tag !!
+                var tbody = $("#prod_table > tbody:last");
 
-                var tr = document.createElement('tr'); 
-                // check if we should highlight a row because price is < setpoint
-                for (var rr in raw_asin) {
-                    if (raw_asin[rr]['Asin'] == returned_data[row]["Code"]) {
-                        setpoint = raw_asin[rr]['Price_Point'];
-                        if (setpoint > returned_data[row]['Original_Price']) {
-                            console.log('You should buy!');
-                            tr.style.backgroundColor = '#d6f5d6'; //aeeaae
+                for (var row in returned_data) {
+                    // log data to webpage
+                    console.log(returned_data[row]);
+
+                    var tr = document.createElement('tr'); 
+                    // check if we should highlight a row because price is < setpoint
+                    for (var rr in raw_asin) {
+                        if (raw_asin[rr]['Asin'] == returned_data[row]["Code"]) {
+                            setpoint = raw_asin[rr]['Price_Point'];
+                            if (setpoint > returned_data[row]['Original_Price']) {
+                                console.log('You should buy!');
+                                tr.style.backgroundColor = '#d6f5d6'; //aeeaae
+                            }
                         }
-                    }
-                }  
+                    }  
 
-                var name = document.createElement('td');
-                var originalPrice = document.createElement('td');
-                var targetPrice = document.createElement('td');
-                var avail = document.createElement('td');
-                var link = document.createElement('td');
-                var del = document.createElement('td');
-                
-                var a = document.createElement('a');
-                a.setAttribute('href','https://www.amazon.com/dp/' + returned_data[row]["Code"]);
-                a.setAttribute('target',"_blank");
-                a.innerHTML = "Product Page";
+                    var name = document.createElement('td');
+                    var originalPrice = document.createElement('td');
+                    var targetPrice = document.createElement('td');
+                    var avail = document.createElement('td');
+                    var link = document.createElement('td');
+                    var del = document.createElement('td');
+                    
+                    var a = document.createElement('a');
+                    a.setAttribute('href','https://www.amazon.com/dp/' + returned_data[row]["Code"]);
+                    a.setAttribute('target',"_blank");
+                    a.innerHTML = "Product Page";
 
-                var b = document.createElement('button');
-                b.setAttribute('id',"delete_button_" + returned_data[row]["Code"]);
-                b.setAttribute('type',"button");
-                b.setAttribute('class',"btn btn-default");
-                b.innerHTML = "Delete"
-                var text1 = document.createTextNode(returned_data[row]["Name"]);
-                var text2 = document.createTextNode("$"+ String(parseFloat(returned_data[row]["Original_Price"]).toFixed(2)));
-                var text3 = document.createTextNode("$"+ String(parseFloat(setpoint.toFixed(2))));
-                var text4 = document.createTextNode(String(returned_data[row]["Availability"]));
+                    var b = document.createElement('button');
+                    b.setAttribute('id',"delete_button_" + returned_data[row]["Code"]);
+                    b.setAttribute('type',"button");
+                    b.setAttribute('class',"btn btn-default");
+                    b.innerHTML = "Delete"
+                    var text1 = document.createTextNode(returned_data[row]["Name"]);
+                    var text2 = document.createTextNode("$"+ String(parseFloat(returned_data[row]["Original_Price"]).toFixed(2)));
+                    var text3 = document.createTextNode("$"+ String(parseFloat(setpoint.toFixed(2))));
+                    var text4 = document.createTextNode(String(returned_data[row]["Availability"]));
 
-                name.appendChild(text1);
-                originalPrice.appendChild(text2);
-                targetPrice.appendChild(text3);
-                avail.appendChild(text4);
-                link.appendChild(a);
-                del.appendChild(b);
+                    name.appendChild(text1);
+                    originalPrice.appendChild(text2);
+                    targetPrice.appendChild(text3);
+                    avail.appendChild(text4);
+                    link.appendChild(a);
+                    del.appendChild(b);
 
-                console.log(b);
+                    console.log(b);
 
-                tr.appendChild(name);
-                tr.appendChild(originalPrice);
-                tr.appendChild(targetPrice);
-                tr.appendChild(avail);
-                tr.appendChild(link);
-                tr.appendChild(del);
+                    tr.appendChild(name);
+                    tr.appendChild(originalPrice);
+                    tr.appendChild(targetPrice);
+                    tr.appendChild(avail);
+                    tr.appendChild(link);
+                    tr.appendChild(del);
 
-                // append the row to the table and move on to the next bit of data
-                tbody.append(tr);
-            }
+                    // append the row to the table and move on to the next bit of data
+                    tbody.append(tr);
+                }
 
-            // handle the delete buttons for each item with wildcard '[id^=delete_button_]'
-            // also this has to be within the ajax calls populating the tags in order to function 
-            $('[id^=delete_button_]').click(function(event) {
-                var raw = event.target.id.split('_');
-                var ASIN = String(raw[2]);
-                console.log("a delete button has been clicked..." + ASIN);
+                // handle the delete buttons for each item with wildcard '[id^=delete_button_]'
+                // also this has to be within the ajax calls populating the tags in order to function 
+                $('[id^=delete_button_]').click(function(event) {
+                    var raw = event.target.id.split('_');
+                    var ASIN = String(raw[2]);
+                    console.log("a delete button has been clicked..." + ASIN);
 
-                var data = {};
-                data.asin = ASIN;
+                    var data = {};
+                    data.asin = ASIN;
 
-                // send the asin back to the server
-                $.ajax({
-                        url: "/delete",
-                        type: "POST",
-                        dataType: "json",
-                        data: JSON.stringify(data),
-                        contentType: "application/json",
+                    // send the asin back to the server
+                    $.ajax({
+                            url: "/delete",
+                            type: "POST",
+                            dataType: "json",
+                            data: JSON.stringify(data),
+                            contentType: "application/json",
 
-                        complete: function() {
-                          //called when complete
-                          console.log('process complete');
-                        },
-                        success: function() {
-                          console.log('process sucess');
-                        },
-                        error: function() {
-                          console.log('process error');
-                        },
-                    });
+                            complete: function() {
+                              //called when complete
+                              console.log('process complete');
+                            },
+                            success: function() {
+                              console.log('process sucess');
+                            },
+                            error: function() {
+                              console.log('process error');
+                            },
+                        });
 
-                // delete the row associated with the button 
-                var R = event.target.parentNode.parentNode.rowIndex;
-                document.getElementById("prod_table").deleteRow(R);
+                    // delete the row associated with the button 
+                    var R = event.target.parentNode.parentNode.rowIndex;
+                    document.getElementById("prod_table").deleteRow(R);
 
-                });
+                    }); // end ajax
 
-            });
+                }); // end get_prodcut_data
+
+        }); // end get_price_point
 
     } // end the create table function brace
 
